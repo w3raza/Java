966e139948cb80b364f30b8f53221bffedc57b6c
w3raza <weraza@vp.pl>
2021-11-25 13:42:15 +0100

    done

---
+++ Task/Interface/Addition.java
@@ -0,0 +1,6 @@
+public class Addition implements Computation {
+
+    public double compute(double arg1, double arg2) {
+        return arg1 + arg2;
+    }
+}
---
+++ Task/Interface/Multiplication.java
@@ -0,0 +1,6 @@
+public class Multiplication implements Computation {
+
+    public double compute(double arg1, double arg2) {
+        return arg1 * arg2;
+    }
+}
