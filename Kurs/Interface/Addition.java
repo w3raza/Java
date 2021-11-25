bdeaabeb2a358f05e520d076f19a2f32ae7d83b3
w3raza <weraza@vp.pl>
2021-11-25 13:40:50 +0100

    changes

--- Kurs/Interface/Addition.java
+++
@@ -1,6 +0,0 @@
-public class Addition implements Computation {
-
-    public double compute(double arg1, double arg2) {
-        return arg1 + arg2;
-    }
-}
--- Kurs/Interface/Computation.java
+++
@@ -1,3 +0,0 @@
-public interface Computation {
-    double compute(double argument1, double argument2);
-}
