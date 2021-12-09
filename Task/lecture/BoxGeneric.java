8b10ac2f52d8b07bbaf98842a3319d29409b61db
w3raza <weraza@vp.pl>
2021-12-09 13:23:45 +0100

    deleted

--- Task/lecture/BoxGeneric.java
+++
@@ -1,7 +0,0 @@
-class BoxGeneric<T> {
-    T obj;
-    BoxGeneric(T x) { obj = x; }
-    T get() { return obj; }
-    String objType() { return obj.getClass().getName(); }
-    }
-    
--- Task/lecture/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main{
-    public static void main(String[] args){
-        BoxGeneric<String> data = new BoxGeneric<String>( "gen" );
-        System.out.println( data.objType() );
-        String s = data.get();
-        // int k = data.get(); /* BLAD kompilacji */
-        System.out.println("s = [" + s + "]");
-    }
-}
